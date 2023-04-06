a1beffdc07c882ebaee78dda85db2e8a5943fba3
tselby81 <106200743+tselby81@users.noreply.github.com>
2023-04-06 18:25:24 -0400

    Input for maze file then show solution

--- DFS_Animation.py
+++ DFS_Animation.py
@@ -15,11 +15,17 @@ with open('solvedSmallMaze.txt', 'r') as f:
     maze = [[char for char in line.strip()] for line in f]
 MAZE = maze
 
+########################################################################################################
+                    # Figure out MAZE_HEIGHT, MAZE_WIDTH
+########################################################################################################
+
 MAZE_HEIGHT = (len(maze)*GRID_SIZE)
 MAZE_WIDTH = (len(maze[0])*GRID_SIZE)
+# CENTER = (MAZE_HEIGHT/2, MAZE_WIDTH/2)
             
-WIN = pygame.display.set_mode((MAZE_WIDTH + (GRID_SIZE*2), MAZE_HEIGHT + (GRID_SIZE*2)))
+WIN = pygame.display.set_mode((MAZE_WIDTH + (GRID_SIZE*4), MAZE_HEIGHT + (GRID_SIZE*4)))
 pygame.display.set_caption("DFS Search")
+
 
 GREY = (174, 179, 189)
 BLACK = (0, 0, 0)
@@ -27,22 +33,15 @@ GREEN = (0, 255, 0)
 LIGHT_BLUE = (95, 180, 245)
 RED = (255, 0, 0)
 
-# Create objects to use to represent different parts of the maze
-# def WALL():
-#     pygame.draw.rect(WIN, BLACK, pygame.Rect((WIDTH/2)-60, (HEIGHT/2)-15, 30, 30))
-# def START():
-#     pygame.draw.rect(WIN, GREEN, pygame.Rect((WIDTH/2)-30, (HEIGHT/2)-15, 30, 30))
-# def PATH():
-#     pygame.draw.rect(WIN, LIGHT_BLUE, pygame.Rect((WIDTH/2), (HEIGHT/2)-15, 30, 30))
-# def GOAL():
-#     pygame.draw.rect(WIN, RED, pygame.Rect((WIDTH/2)+30, (HEIGHT/2)-15, 30, 30))
+LIGHT_PURPLE = (159, 121, 238)
+DARK_PURPLE = (93, 71, 139)
 
-FPS = 0.5
+FPS = 30
 
 
 def draw_grid(surface):
-    for y in range(0, int(MAZE_HEIGHT + GRID_SIZE)):
-        for x in range(0, int(MAZE_WIDTH + GRID_SIZE)):
+    for y in range(0, int(MAZE_HEIGHT + GRID_SIZE*4)):
+        for x in range(0, int(MAZE_WIDTH + GRID_SIZE*4)):
             if (x+y) % 2 == 0:
                 r = pygame.Rect((x * GRID_SIZE, y * GRID_SIZE), (GRID_SIZE, GRID_SIZE))
                 pygame.draw.rect(surface, pygame.Color("lightslategrey"), r)
@@ -56,25 +55,22 @@ def draw_window():
 
     draw_grid(WIN)
 
-
-
     with open('solvedSmallMaze.txt', 'r') as f:
-            #file = f.read()
-            #maze = [list(line.strip()) for line in file.split('\n')]
             maze = [[char for char in line.strip()] for line in f]
     for row in range(len(maze)):
         for col in range(len(maze[row])):
-            if maze[row][col] == 'P':
-                maze[row][col] = pygame.draw.rect(WIN, GREEN, pygame.Rect((col*GRID_SIZE)+GRID_SIZE, (row*GRID_SIZE)+GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            if maze[row][col] == '%':
+                maze[row][col] = pygame.draw.rect(WIN, BLACK, pygame.Rect((col*GRID_SIZE)+(GRID_SIZE*2), (row*GRID_SIZE)+(GRID_SIZE*2), GRID_SIZE, GRID_SIZE))
 
-            elif maze[row][col] == 'G':
-                maze[row][col] = pygame.draw.rect(WIN, RED, pygame.Rect((col*GRID_SIZE)+GRID_SIZE, (row*GRID_SIZE)+GRID_SIZE, GRID_SIZE, GRID_SIZE))
-
-            elif maze[row][col] == '%':
-                maze[row][col] = pygame.draw.rect(WIN, BLACK, pygame.Rect((col*GRID_SIZE)+GRID_SIZE, (row*GRID_SIZE)+GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            elif maze[row][col] == 'P':
+                maze[row][col] = pygame.draw.rect(WIN, GREEN, pygame.Rect((col*GRID_SIZE)+(GRID_SIZE*2), (row*GRID_SIZE)+(GRID_SIZE*2), GRID_SIZE, GRID_SIZE))
 
             elif maze[row][col] == '.':
-                maze[row][col] = pygame.draw.rect(WIN, LIGHT_BLUE, pygame.Rect((col*GRID_SIZE)+GRID_SIZE, (row*GRID_SIZE)+GRID_SIZE, GRID_SIZE, GRID_SIZE))
+                maze[row][col] = pygame.draw.rect(WIN, LIGHT_BLUE, pygame.Rect((col*GRID_SIZE)+(GRID_SIZE*2), (row*GRID_SIZE)+(GRID_SIZE*2), GRID_SIZE, GRID_SIZE))
+
+            elif maze[row][col] == 'G':
+                maze[row][col] = pygame.draw.rect(WIN, RED, pygame.Rect((col*GRID_SIZE)+(GRID_SIZE*2), (row*GRID_SIZE)+(GRID_SIZE*2), GRID_SIZE, GRID_SIZE))
+
 
 #Need to update the window to display what has been drawn in the loop
         pygame.display.update()
@@ -84,7 +80,7 @@ def choose_maze():
     pygame.init()
 
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((MAZE_WIDTH + (GRID_SIZE*2), MAZE_HEIGHT + (GRID_SIZE*2)))
+    screen = pygame.display.set_mode((MAZE_WIDTH + (GRID_SIZE*4), MAZE_HEIGHT + (GRID_SIZE*4)))
     pygame.display.set_caption("FILE SELECT")
 
     surface = pygame.Surface(screen.get_size())
@@ -92,33 +88,70 @@ def choose_maze():
 
     font = pygame.font.SysFont('times', 30)
     font2 = pygame.font.SysFont('maiandragd', 18)
+
+    input = pygame.Rect(200, 100, 300, 30)
+    input.center = ((MAZE_WIDTH + (GRID_SIZE*4)) / 2, 190)
+
+    user_text = ""
+    
     intro = True
+    active = False
 
     while intro:
+        if active:
+            color = LIGHT_PURPLE
+        else:
+            color = DARK_PURPLE
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                intro = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input.collidepoint(event.pos):
+                    active = True
+                else:
+                    active = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
+                    user_text = ""
                     intro = False
+                    return user_text
+                if event.key == pygame.K_BACKSPACE:
+                    # get text input from 0 to -1 i.e. end.
+                    user_text = user_text[:-1]
+                else:
+                    user_text += event.unicode
+            elif event.type == pygame.QUIT:
+                intro = False
+                # sys.exit()
+        #return user_text
 
         clock.tick(FPS)
 
         screen.blit(surface, (0, 0))
-        screen.fill(GREY)
+        screen.fill("lightslategrey")
         pygame.display.flip()
 
-        title = font.render("Please enter maze file name below", True, (0, 0, 0), None)
+        title = font.render("PLEASE ENTER MAZE FILE NAME BELOW.", True, (0, 0, 0), None)
         titleRect = title.get_rect()
-        titleRect.center = (MAZE_WIDTH / 2, 50)
+        titleRect.center = ((MAZE_WIDTH + (GRID_SIZE*4)) / 2, 125)
         screen.blit(title, titleRect)
 
-        subtitle = font2.render("INSERT TEXT BOX HERE", True, (0, 0, 0), None)
+        example = font.render("(sampleFile.txt).", True, (0, 0, 0), None)
+        exampleRect = example.get_rect()
+        exampleRect.center = ((MAZE_WIDTH + (GRID_SIZE*4)) / 2, 155)
+        screen.blit(example, exampleRect)
+
+
+        pygame.draw.rect(screen, color, input)
+        text_surface = font2.render(user_text, True, (255, 255, 255))
+        screen.blit(text_surface, (input.x+5, input.y+5))
+        pygame.draw.rect(screen, BLACK, (input), 2)
+
+        subtitle = font.render("PRESS ENTER WHEN FINISHED.", True, (0, 0, 0), None)
         subtitleRect = subtitle.get_rect()
-        subtitleRect.center = (MAZE_WIDTH / 2, 90)
+        subtitleRect.center = ((MAZE_WIDTH + (GRID_SIZE*4)) / 2, 230)
         screen.blit(subtitle, subtitleRect)
 
         pygame.display.update()
+    return user_text
 """
 Main Game Loop.
 This should hold things related to game logic.
@@ -126,12 +159,8 @@ Other specialized functions should be written outside and called in the game loo
 """
 def main():
 
-    # maze = open('solvedBigMaze.txt')
-    # print(maze.read())
-
     # Clock item to control how many times we loop per second
     clock = pygame.time.Clock()
-
 
     run = True
     while run:
